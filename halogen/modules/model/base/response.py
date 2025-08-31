0548c0b25ff9fcdaf48484fa87893211af3081f9
haseeb <haseeeb8j@gmail.com>
2025-08-31 16:48:24 +0500

    Adding task groups for efficient multi tasking

--- halogen/modules/model/base/response.py
+++ halogen/modules/model/base/response.py
@@ -2,17 +2,25 @@ from typing import Any
 from pydantic import BaseModel
 
 
+
 class ModelTask(BaseModel):
 	namespace: str
 	task_name: str
 	args: list[str]
 
+
+class ModelTaskGroup(BaseModel):
+	name: str
+	tasks: list[ModelTask]
+
+
 class ModelExtras(BaseModel):
 	key: str
 	value: Any
 
+
 class ModelResponse(BaseModel):
 	"Use this class if the model can be configured using schema directly."
 	message: str
-	tasks: list[ModelTask] 
+	tasks_groups: list[ModelTaskGroup]
 	extras: list[ModelExtras]
